3e1e39cbd7bc774700b33682f447a385094cf8b1
Elaman1121 <elamaneralievv@gmail.com>
2025-05-11 06:38:10 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -133,4 +133,4 @@ def index():
 
 # Port Heroku автоматты түрде орнатады
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False) 
